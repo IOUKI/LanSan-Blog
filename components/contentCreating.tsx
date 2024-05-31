cbaa334815f93f385cd8effffb9968b6a8d18dd4
Lan <46338530+IOUKI@users.noreply.github.com>
2024-05-31 16:46:19 +0800

    add: 製作中文字 component

---
+++ components/contentCreating.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContentCreating = () => {
+  return (
+    <section className="w-full mt-56 text-center text-2xl">
+      <p>網頁內容龜速建立中...敬請期待&gt;&lt;</p>
+    </section>
+  )
+}
+
+export default ContentCreating
